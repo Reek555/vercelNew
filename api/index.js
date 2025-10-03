e3cdb95dccfcfa22762059b34562c270ff093882
rick <taariq.connect@gmail.com>
2025-10-03 22:41:41 +0100

    lll

--- api/index.js
+++ api/index.js
@@ -1,13 +1,11 @@
 // Use "type: module" in package.json to use ES modules
 import express from 'express';
 const app = express();
-const port = 3000;
  
 // Define your routes
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
 });
  
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// Export the Express app
+export default app;
