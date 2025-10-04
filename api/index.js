8ba5d292f5da2e4a614abc57ad839a77552621a9
rick <taariq.connect@gmail.com>
2025-10-04 15:54:51 +0100

    lll

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 // Use "type: module" in package.json to use ES modules
 import express from 'express';
+import sharp from "sharp"
+import { createCanvas } from 'canvas';
+
 const app = express();
  
 // Define your routes
@@ -10,6 +13,44 @@ app.get('/', (req, res) => {
 app.get('/home', (req, res) => {
   res.send("welcome home.");
 });
+
+
+app.get("/test", (req, res) => {
+
+    res.setHeader('Content-Disposition', 'attachment; filename="image.png"');  //the name of image file
+    res.setHeader('Content-Type', 'image/png'); // Set response type
+
+
+
+    const canvas = createCanvas(736, 1104)
+    const ctx = canvas.getContext('2d')
+
+    //ctx.font = 'bold 80px "Cairo, Regular"'
+    ctx.fillStyle = '#f9d697'; 
+    ctx.textAlign = 'center';
+    ctx.fillText('lorem ipsum', canvas.width / 2, 280)
+    //ctx.font = '40px "Cairo, Regular" '
+    ctx.fillText("lorem ipsum", canvas.width / 2, 580)
+    
+        
+    
+    sharp(canvas.toBuffer('image/png'))
+    .png()
+    .pipe(res)
+
+
+
+
+
+})
  
 // Export the Express app
 export default app;
+
+/* 
+app.listen( 3000 , () => {
+  console.log ("server is ready")
+})
+ */
+
+
